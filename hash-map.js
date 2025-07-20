a0a3d8d6b9b52f8479a4b684a07957dd7152da06
Key Feula <keyfeula1214@gmail.com>
2025-07-19 18:36:52 -0700

    Add get and set methods for hash table

--- hash-map.js
+++ hash-map.js
@@ -1,17 +1,26 @@
 function createHashMap(loadFactor = 0.8, capacity = 16) {
-    return {
-        hash(key) {
-            let hashCode = 0;
+    const array = new Array(capacity).fill(new Array());
+
+    function hash(key) {
+        let hashCode = 0;
             const primeNumber = 31;
             for (let i = 0; i < key.length; i++) {
                 hashCode = primeNumber * hashCode + key.charCodeAt(i);
                 hashCode = hashCode % capacity;
             }
             return hashCode;
-        },
+    }
 
+    return {
+        set(key, value) {
+            array[hash(key)] = { key, value };
+        },
+        get(key) {
+            return array[hash(key)].value;
+        }
     }
 }
 
 let map = createHashMap();
-console.log(map.hash("keyydfsdfv"));
+map.set("key", "hello");
+console.log(map.get("key"));
